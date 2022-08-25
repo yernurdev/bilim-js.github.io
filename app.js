9ba415958b7573f8e6dcd7746606dba388e798c6
yernur syrlibayev <107348190+yernurdev@users.noreply.github.com>
2022-08-25 13:48:37 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
+let progress=document.getElementById('progressbar');
+let totalHeight=document.body.scrollHeight=window.innerHeight;
+window.onscroll=function(){
+  let progressHeight=(window.pageYOffset / totalHeight) * 100;
+  progress.style.height=progressHeight+"%";
+}
+
+//theme
+
+
+const themeMap = {
+  dark: "light",
+  light: "solar",
+  solar: "dark"
+};
+
+const theme = localStorage.getItem('theme')
+  || (tmp = Object.keys(themeMap)[0],
+      localStorage.setItem('theme', tmp),
+      tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem('theme');
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem('theme', next);
+}
+
+document.getElementById('themeButton').onclick = toggleTheme;
